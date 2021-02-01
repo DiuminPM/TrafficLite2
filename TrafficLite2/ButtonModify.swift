f7db534faa957e2c4a4a44ecedaab929a28944c2
DiuminPM <75372207+DiuminPM@users.noreply.github.com>
2021-02-01 15:08:31 +0300

    final

--- TrafficLite2/ButtonModify.swift
+++ TrafficLite2/ButtonModify.swift
@@ -8,13 +8,28 @@
 import SwiftUI
 
 struct ButtonModify: View {
+    let title: String
+    let action:() -> Void
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Button(title, action: action)
+        .font(.title)
+        .frame(width: 300, height: 50)
+        .background(Color.blue)
+        .cornerRadius(20)
+        
+        .overlay(
+            RoundedRectangle(cornerRadius: 20)
+                .stroke(Color.white, lineWidth: 4)
+                )
+        .foregroundColor(.white)
+                    .padding()
+                }
     }
-}
+
 
 struct ButtonModify_Previews: PreviewProvider {
     static var previews: some View {
-        ButtonModify()
-    }
+        ButtonModify(title: "START", action: {})
+        
+        }
 }
--- TrafficLite2/ContentView.swift
+++ TrafficLite2/ContentView.swift
@@ -8,10 +8,11 @@
 import SwiftUI
 
 struct TrafficLite: View {
+    
     enum EnumTrufficLight {
         case red, yellow, green
     }
-    
+    @State private var titleButton = "START"
     @State private var alfaRed = 0.3
     @State private var alfaYellow = 0.3
     @State private var alfaGreen = 0.3
@@ -23,29 +24,30 @@ struct TrafficLite: View {
             YellowLight(alphaYellow: alfaYellow)
             GreenLight(alphaGreen: alfaGreen)
             Spacer()
-            Button("NEXT", action: {
-                switched()
-            })
-            .font(.title)
-            .frame(width: 300, height: 50)
-            .background(Color.blue)
-            .cornerRadius(20)
-            
-            .overlay(
-                RoundedRectangle(cornerRadius: 20)
-                    .stroke(Color.white, lineWidth: 4)
-                    )
-            .foregroundColor(.white)
-                        .padding()
-                    }
+            ButtonModify(title: titleButton, action: switched )
+//            Button("NEXT", action: {
+//                switched()
+//            })
+//            .font(.title)
+//            .frame(width: 300, height: 50)
+//            .background(Color.blue)
+//            .cornerRadius(20)
+//
+//            .overlay(
+//                RoundedRectangle(cornerRadius: 20)
+//                    .stroke(Color.white, lineWidth: 4)
+//                    )
+//            .foregroundColor(.white)
+//                        .padding()
+//                    }
+
+        }
         .padding(.top, 80 )
         .padding(.bottom, 40)
         
         .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity)
         .background(Color.black)
         .ignoresSafeArea()
-        
-        }
     
 }
 
@@ -54,9 +56,11 @@ struct ContentView_Previews: PreviewProvider {
         TrafficLite()
     }
 }
+}
 
 extension TrafficLite {
     func switched() {
+        titleButton = "NEXT"
         switch (enumLight) {
         case .red:
             alfaGreen = 0.3
